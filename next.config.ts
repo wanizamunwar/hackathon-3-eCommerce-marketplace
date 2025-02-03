b83fa61c3591f5650aa440fe89c52af007dcb2cc
wanizamunwar <wanizamunwar03@gmail.com>
2025-02-04 03:18:11 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: ['plus.unsplash.com', "images.unsplash.com", 'cdn.sanity.io']
+  },
+};
+
+export default nextConfig;
